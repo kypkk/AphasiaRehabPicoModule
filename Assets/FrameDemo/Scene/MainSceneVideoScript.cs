974c83ea63ef1bea56157a1fa30883e00f76663f
kypkk <kypss50102@gmail.com>
2023-03-13 17:31:07 +0800

    feat: clip list

--- Assets/FrameDemo/Scene/MainSceneVideoScript.cs
+++ Assets/FrameDemo/Scene/MainSceneVideoScript.cs
@@ -8,11 +8,18 @@ public class MainSceneVideoScript : MonoBehaviour
 {
     public Text hint; 
     public VideoClip problemClip;
+    public VideoPlayer problemPlayer;
+    public VideoClip[] easy_problem;
+    public VideoClip[] medium_problem;
+    public VideoClip[] hard_problem;
+
+
     
     void Start()
     {
         string hintText = problemClip.ToString().Replace("(UnityEngine.VideoClip)", "").Replace("1-","");
         hint.text = hintText;
+        problemPlayer.clip = problemClip;
     }
 
     // Update is called once per frame
@@ -20,4 +27,5 @@ public class MainSceneVideoScript : MonoBehaviour
     {
         
     }
+    
 }
